6372fbcd00d74afd0c716059f8a390a3b00e9324
Kostya <kosmat3101@gmail.com>
2022-06-27 15:09:52 +0300

    full

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
+const refs = {
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]'),
+    body:document.querySelector('body'),
+}
 
+const DELAY = 1000;
+
+refs.btnStart.addEventListener('click', onStart);
+
+refs.btnStop.addEventListener('click', onStop);
+
+refs.btnStop.disabled = true;
+let intervelId = null;
+function onStart() {
+    
+    refs.btnStart.disabled = true;
+    refs.btnStop.disabled = false;
+    changeColor()
+
+}
+
+function onStop() {
+    refs.btnStop.disabled = true;
+    refs.btnStart.disabled = false;
+    clearInterval(intervelId);
+}
+
+function changeColor() {
+     intervelId = setInterval(() => {
+        let color = getRandomHexColor();
+        refs.body.style.background= color
+    },DELAY)
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+const refs = {
+  form: document.querySelector('form'),
+  firstStep: document.querySelector('input[name="delay"]'),
+  delayStep: document.querySelector('input[name="step"]'),
+  amount: document.querySelector('input[name="amount"]')
+};
+console.log(refs.firstStep.value,refs.delayStep,refs.amount)
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({position, delay});
+      }
+    },delay)
   }
+ )
 }
+
+refs.form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  console.log();
+  let time = Number(refs.firstStep.value);
+  for (let i = 1; i <= Number(refs.amount.value); i++){
+    createPromise(i,time)
+  .then(({ position, delay } ) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+    time += Number(refs.delayStep.value);
+  }
+  e.currentTarget.reset()
+})
+
